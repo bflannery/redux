7b15abf2683b04eb4e4adb8c6027614f3cf829c8
Brian Flannery <bflannery66@gmail.com>
2017-06-16 08:00:11 -0500

    store methods

--- client/index.js
+++ client/index.js
@@ -1,5 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import expect from 'expect';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const counter = (state = 0, action) => {
+   switch (action.type) {
+     case 'INCREMENT':
+        return state + 1;
+     case 'DECREMENT':
+        return state - 1;
+     default:
+        return state;
+   }
+}
+
+
+const store = createStore(counter);
+
+
+const render = () => {
+  document.body.innerText = store.getState();
+};
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT'});
+});
+//
+// expect(
+//   counter(0, { type: 'INCREMENT'})
+// ).toEqual(1);
+//
+// expect(
+//   counter(1, { type: 'INCREMENT'})
+// ).toEqual(2);
+//
+// expect(
+//   counter(2, { type: 'DECREMENT'})
+// ).toEqual(1);
+//
+// expect(
+//   counter(1, { type: 'DECREMENT'})
+// ).toEqual(0);
+//
+// expect(
+//   counter(1, { type: 'SOMETHING_ELSE' })
+// ).toEqual(1);
+//
+// expect(
+//   counter(undefined, {})
+// ).toEqual(0);
+//
+// console.log('Tests passed!');
